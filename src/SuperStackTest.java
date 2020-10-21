e0781fc7fc2c550e664eed5b2eb7009b1653e362
Avery <avery.moates17@ou.edu>
2020-10-21 16:57:45 -0500

    Added test cases for the SuperStack class.

---
+++ src/SuperStackTest.java
@@ -0,0 +1,42 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class SuperStackTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@SuppressWarnings("deprecation")
+	@Test
+	void testIsEmpty() {
+		// create an empty stack and check if it is empty
+		SuperStack<Integer> stack = new SuperStack<Integer>(1);
+		
+		boolean result = stack.isEmpty();
+		assertTrue(result);
+		
+		result = stack.push(new Integer(0));
+		assertFalse(result);
+		
+		// add a few elemtents and check if it is empty
+	}
+
+	@Test
+	void testIsFull() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testPush() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testToString() {
+		fail("Not yet implemented");
+	}
+
+}
